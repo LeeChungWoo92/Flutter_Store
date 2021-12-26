4a9d964cd0eb0461c74855ff750e685fd0d84cad
LeeChungWoo92 <skcjddn@gmail.com>
2021-12-26 22:53:39 +0900

    Add image

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,23 @@ class StorePage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-                Text("Woman",style: TextStyle(fontWeight: FontWeight.bold)),
-                Spacer(),
-                Text("Kids",style: TextStyle(fontWeight: FontWeight.bold)),
-                Spacer(),
-                Text("Shoes",style: TextStyle(fontWeight: FontWeight.bold)),
-                Spacer(),
-                Text("Bag",style: TextStyle(fontWeight: FontWeight.bold)),
-              ],
-            )
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                children: [
+                  Text("Woman",style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Kids",style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Shoes",style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Bag",style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+            Expanded(child: Image.asset("assets/bag.jpeg",fit:BoxFit.cover)),
+            SizedBox(height: 2),
+            Expanded(child: Image.asset("assets/cloth.jpeg",fit:BoxFit.cover)),
           ],
         ),
       ),
